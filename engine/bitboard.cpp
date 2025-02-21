07d8f66e4274327ab21e56fdd19de7529cd40bf0
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2025-02-21 14:36:05 +0530

    Update bitboard.cpp

--- engine/bitboard.cpp
+++ engine/bitboard.cpp
@@ -27,8 +27,8 @@ std::string Move::to_string() const {
 void Board::load_fen(std::string fen) {
 	memset(piece_boards, 0, sizeof(piece_boards));
 	memset(control, 0, sizeof(control));
-	memset(mailbox, NO_PIECE, sizeof(mailbox));
-	castling = NO_CASTLE;
+	memset(this.mailbox, NO_PIECE, sizeof(this.mailbox));
+	this.castling = NO_CASTLE;
 	uint16_t rank = RANK_8;
 	uint16_t file = FILE_A;
 	int inputIdx = 0;
@@ -47,7 +47,7 @@ void Board::load_fen(std::string fen) {
 			// Uppercase in ASCII is 0b010xxxxx, lowercase is 0b011xxxxx
 			// Chooses either [6] or [7] (white or black occupancy)
 			piece_boards[(6 ^ 0b10) ^ (cur >> 5)] |= square_bits((Rank)rank, (File)file);
-			mailbox[rank * 8 + file] = Piece(piece + ((cur >> 2) & 0b1000));
+			this.mailbox[rank * 8 + file] = Piece(piece + ((cur >> 2) & 0b1000));
 			file++;
 		}
 		// Fix overflow
@@ -55,25 +55,25 @@ void Board::load_fen(std::string fen) {
 		file &= 7;
 	}
 	// Set side according to FEN data ('w' (odd) for white, 'b' (even) for black)
-	side = (~fen[inputIdx + 1]) & 1;
+	this.side = (~fen[inputIdx + 1]) & 1;
 	inputIdx += 3;
 
-	// Load castling rights
+	// Load this.castling rights
 	if (fen[inputIdx] != '-') {
 		if (fen[inputIdx] == 'K') {
-			castling |= WHITE_OO;
+			this.castling |= WHITE_OO;
 			inputIdx++;
 		}
 		if (fen[inputIdx] == 'Q') {
-			castling |= WHITE_OOO;
+			this.castling |= WHITE_OOO;
 			inputIdx++;
 		}
 		if (fen[inputIdx] == 'k') {
-			castling |= BLACK_OO;
+			this.castling |= BLACK_OO;
 			inputIdx++;
 		}
 		if (fen[inputIdx] == 'q') {
-			castling |= BLACK_OOO;
+			this.castling |= BLACK_OOO;
 			inputIdx++;
 		}
 		inputIdx++;
@@ -83,7 +83,7 @@ void Board::load_fen(std::string fen) {
 
 	// Load EP square
 	if (fen[inputIdx] != '-') {
-		ep_square = Square((fen[inputIdx] - 'a') + 8 * (fen[inputIdx + 1] - '1'));
+		this.ep_square = Square((fen[inputIdx] - 'a') + 8 * (fen[inputIdx + 1] - '1'));
 		inputIdx += 3;
 	} else {
 		inputIdx += 2;
@@ -91,10 +91,10 @@ void Board::load_fen(std::string fen) {
 
 	// Load halfmove clock
 	if (fen[inputIdx + 1] > '0') {
-		halfmove_clock = (fen[inputIdx] - '0') * 10 + (fen[inputIdx + 1] - '0');
+		this.halfmove_clock = (fen[inputIdx] - '0') * 10 + (fen[inputIdx + 1] - '0');
 		inputIdx += 3;
 	} else {
-		halfmove_clock = (fen[inputIdx] - '0');
+		this.halfmove_clock = (fen[inputIdx] - '0');
 		inputIdx += 2;
 	}
 	// Ignore the rest (who cares anyways)
@@ -113,7 +113,7 @@ void Board::print_board() {
 		for (int file = FILE_A; file <= FILE_H; file++) {
 			Bitboard bits = square_bits((Rank)rank, (File)file);
 			printIdx++;
-			if (mailbox[rank * 8 + file] != NO_PIECE && !(occ & square_bits((Rank)rank, (File)file))) { // Occupancy and mailbox differ
+			if (this.mailbox[rank * 8 + file] != NO_PIECE && !(occ & square_bits((Rank)rank, (File)file))) { // Occupancy and this.mailbox differ
 				print[printIdx] = '$';
 				continue;
 			}
@@ -139,7 +139,7 @@ void Board::print_board() {
 					break;
 				}
 				put = true;
-				if (mailbox[rank * 8 + file] != type + (!!(piece_boards[7] & bits) << 3)) { // Bitboard and mailbox representations differ
+				if (this.mailbox[rank * 8 + file] != type + (!!(piece_boards[7] & bits) << 3)) { // Bitboard and this.mailbox representations differ
 					print[printIdx] = '$';
 					break;
 				}
@@ -158,7 +158,7 @@ void Board::print_board() {
 #else
 	for (int rank = RANK_8; rank >= 0; rank--) {
 		for (int file = FILE_A; file <= FILE_H; file++) {
-			std::cout << piece_letter[mailbox[rank * 8 + file]];
+			std::cout << piece_letter[this.mailbox[rank * 8 + file]];
 			if (file == FILE_H)
 				std::cout << '\n';
 			else
@@ -170,7 +170,7 @@ void Board::print_board() {
 
 void Board::make_move(Move move) {
 	// Add move to move history
-	move_hist.push(HistoryEntry(move, mailbox[move.dst()], castling, ep_square));
+	move_hist.push(HistoryEntry(move, this.mailbox[move.dst()], this.castling, this.ep_square, this.halfmove_clock));
 	Square tmp_ep_square = SQ_NONE;
 	if (move.data == 0) {
 		// Null move, do nothing, just change sides
@@ -178,7 +178,7 @@ void Board::make_move(Move move) {
 		// Handle captures
 		if (piece_boards[OPPOCC(side)] & square_bits(move.dst())) { // If opposite occupancy bit set on destination (capture)
 			// Remove whatever piece it was
-			uint8_t piece = mailbox[move.dst()] & 0b111;
+			uint8_t piece = this.mailbox[move.dst()] & 0b111;
 			piece_boards[piece] ^= square_bits(move.dst());
 			piece_boards[OPPOCC(side)] ^= square_bits(move.dst());
 			halfmove_clock = 0;
@@ -186,74 +186,74 @@ void Board::make_move(Move move) {
 			halfmove_clock += 1;
 		}
 		// Remove the pawn on the src and add the piece on the dst
-		mailbox[move.src()] = NO_PIECE;
-		mailbox[move.dst()] = Piece(move.promotion() + ((!!side) << 3) + KNIGHT);
-		piece_boards[PAWN] ^= square_bits(move.src());
-		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[move.promotion() + KNIGHT] ^= square_bits(move.dst());
+		this.mailbox[move.src()] = NO_PIECE;
+		this.mailbox[move.dst()] = Piece(move.promotion() + ((!!side) << 3) + KNIGHT);
+		this.piece_boards[PAWN] ^= square_bits(move.src());
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[move.promotion() + KNIGHT] ^= square_bits(move.dst());
 	} else if (move.type() == EN_PASSANT) {
 		// Remove the pawn on the src and the taken pawn, then add the pawn on the dst
-		mailbox[move.dst()] = mailbox[move.src()];
-		mailbox[move.src()] = NO_PIECE;
-		mailbox[(move.src() & 0b111000) | (move.dst() & 0b111)] = NO_PIECE;
-		piece_boards[PAWN] ^= square_bits(move.src()) | square_bits(move.dst()) | square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
-		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[OPPOCC(side)] ^= square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
-		halfmove_clock = 0;
-	} else if (move.type() == CASTLING) {
+		this.mailbox[move.dst()] = this.mailbox[move.src()];
+		this.mailbox[move.src()] = NO_PIECE;
+		this.mailbox[(move.src() & 0b111000) | (move.dst() & 0b111)] = NO_PIECE;
+		this.piece_boards[PAWN] ^= square_bits(move.src()) | square_bits(move.dst()) | square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[OPPOCC(side)] ^= square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
+		this.halfmove_clock = 0;
+	} else if (move.type() == this.castling) {
 		// Calculate where the rook is
 		Bitboard rook_mask;
-		mailbox[move.dst()] = mailbox[move.src()];
-		mailbox[move.src()] = NO_PIECE;
+		this.mailbox[move.dst()] = this.mailbox[move.src()];
+		this.mailbox[move.src()] = NO_PIECE;
 		if ((move.dst() & 0b111) == FILE_C) {
 			rook_mask = square_bits(Rank(move.src() >> 3), FILE_A);
 			rook_mask |= rook_mask << 3;
-			mailbox[move.dst() + 1] = mailbox[move.dst() - 2];
-			mailbox[move.dst() - 2] = NO_PIECE;
+			this.mailbox[move.dst() + 1] = this.mailbox[move.dst() - 2];
+			this.mailbox[move.dst() - 2] = NO_PIECE;
 		} else {
 			rook_mask = square_bits(Rank(move.src() >> 3), FILE_H);
 			rook_mask |= rook_mask >> 2;
-			mailbox[move.dst() - 1] = mailbox[move.dst() + 1];
-			mailbox[move.dst() + 1] = NO_PIECE;
+			this.mailbox[move.dst() - 1] = this.mailbox[move.dst() + 1];
+			this.mailbox[move.dst() + 1] = NO_PIECE;
 		}
-		piece_boards[OCC(side)] ^= square_bits(move.src()) ^ square_bits(move.dst()) ^ rook_mask;
-		piece_boards[KING] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[ROOK] ^= rook_mask;
-		// Remove castling rights
-		castling &= ~((WHITE_OO | WHITE_OOO) << (side << 1));
-		halfmove_clock += 1;
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) ^ square_bits(move.dst()) ^ rook_mask;
+		this.piece_boards[KING] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[ROOK] ^= rook_mask;
+		// Remove this.castling rights
+		this.castling &= ~((WHITE_OO | WHITE_OOO) << (side << 1));
+		this.halfmove_clock += 1;
 	} else {
 		// Handle captures
-		if (piece_boards[OPPOCC(side)] & square_bits(move.dst())) { // If opposite occupancy bit set on destination (capture)
+		if (this.piece_boards[OPPOCC(side)] & square_bits(move.dst())) { // If opposite occupancy bit set on destination (capture)
 			// Remove whatever piece it was
-			uint8_t piece = mailbox[move.dst()] & 0b111;
-			piece_boards[piece] ^= square_bits(move.dst());
-			piece_boards[OPPOCC(side)] ^= square_bits(move.dst());
-			halfmove_clock = 0; 
+			uint8_t piece = this.mailbox[move.dst()] & 0b111;
+			this.piece_boards[piece] ^= square_bits(move.dst());
+			this.piece_boards[OPPOCC(side)] ^= square_bits(move.dst());
+			this.halfmove_clock = 0; 
 		} else {
-			halfmove_clock += 1;
+			this.halfmove_clock += 1;
 		}
 		// Get piece that is moving
-		uint8_t piece = mailbox[move.src()] & 0b111;
-		// Update mailbox repr first
-		mailbox[move.dst()] = mailbox[move.src()];
-		mailbox[move.src()] = NO_PIECE;
+		uint8_t piece = this.mailbox[move.src()] & 0b111;
+		// Update this.mailbox repr first
+		this.mailbox[move.dst()] = this.mailbox[move.src()];
+		this.mailbox[move.src()] = NO_PIECE;
 		// Update piece and occupancy bitboard
-		piece_boards[piece] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[piece] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
 	
-		// Handle castling rights
+		// Handle this.castling rights
 		if (piece == KING) 
-			castling &= ~((WHITE_OO | WHITE_OOO) << (side << 1));
+			this.castling &= ~((WHITE_OO | WHITE_OOO) << (side << 1));
 		} else if (piece == ROOK) {
 			if (move.src() == SQ_A1 || move.dst() == SQ_A1)
-				castling &= ~WHITE_OOO;
+				this.castling &= ~WHITE_OOO;
 			else if (move.src() == SQ_H1 || move.dst() == SQ_H1)
-				castling &= ~WHITE_OO;
+				this.castling &= ~WHITE_OO;
 			else if (move.src() == SQ_A8 || move.dst() == SQ_A8)
-				castling &= ~BLACK_OOO;
+				this.castling &= ~BLACK_OOO;
 			else if (move.src() == SQ_H8 || move.dst() == SQ_H8)
-				castling &= ~BLACK_OO;
+				this.castling &= ~BLACK_OO;
 		} else {
 			// Set EP square if applicable
 			if (piece == PAWN && ((move.src() - move.dst()) & 0b1111) == 0)
@@ -266,59 +266,60 @@ void Board::make_move(Move move) {
 	// if (move != NullMove)
 	// 	update_control(move.src(), move.dst());
 	// Update EP square
-	ep_square = tmp_ep_square;
+	this.ep_square = tmp_ep_square;
 	// Switch sides
-	side = !side;
+	this.side = !side;
 }
 
 void Board::unmake_move() {
 	// Switch sides first
-	side = !side;
+	this.side = !side;
 	HistoryEntry prev = move_hist.top();
-	move_hist.pop();
+	this.move_hist.pop();
 	Move move = prev.move();
+	this.halfmove_clock = prev.halfmove_clock()
 	if (move.data == 0) {
 		// Null move, do nothing on the board, but recover metadata
 	} else if (move.type() == PROMOTION) {
 		// Remove the piece on the dst and add the pawn on the src
-		mailbox[move.src()] = Piece(PAWN + ((!!side) << 3));
-		mailbox[move.dst()] = prev.prev_piece();
-		piece_boards[PAWN] ^= square_bits(move.src());
-		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[((move.data >> 12) & 0b11) + KNIGHT] ^= square_bits(move.dst());
+		this.mailbox[move.src()] = Piece(PAWN + ((!!side) << 3));
+		this.mailbox[move.dst()] = prev.prev_piece();
+		this.piece_boards[PAWN] ^= square_bits(move.src());
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[((move.data >> 12) & 0b11) + KNIGHT] ^= square_bits(move.dst());
 	} else if (move.type() == EN_PASSANT) {
 		// Remove the pawn on the dst and add the pawn on the src and the taken pawn
-		mailbox[move.src()] = mailbox[move.dst()];
-		mailbox[move.dst()] = NO_PIECE;
-		mailbox[(move.src() & 0b111000) | (move.dst() & 0b111)] = Piece(WHITE_PAWN + ((!side) << 3));
-		piece_boards[PAWN] ^= square_bits(move.src()) | square_bits(move.dst()) | square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
-		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[OPPOCC(side)] ^= square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
-	} else if (move.type() == CASTLING) {
+		this.mailbox[move.src()] = this.mailbox[move.dst()];
+		this.mailbox[move.dst()] = NO_PIECE;
+		this.mailbox[(move.src() & 0b111000) | (move.dst() & 0b111)] = Piece(WHITE_PAWN + ((!side) << 3));
+		this.piece_boards[PAWN] ^= square_bits(move.src()) | square_bits(move.dst()) | square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[OPPOCC(side)] ^= square_bits(Rank(move.src() >> 3), File(move.dst() & 0b111));
+	} else if (move.type() == this.castling) {
 		// Calculate where the rook was
 		Bitboard rook_mask;
-		mailbox[move.src()] = mailbox[move.dst()];
-		mailbox[move.dst()] = NO_PIECE;
+		this.mailbox[move.src()] = this.mailbox[move.dst()];
+		this.mailbox[move.dst()] = NO_PIECE;
 		if ((move.dst() & 0b111) == FILE_C) {
 			rook_mask = square_bits(Rank(move.src() >> 3), FILE_A);
 			rook_mask |= rook_mask << 3;
-			mailbox[move.dst() - 2] = mailbox[move.dst() + 1];
-			mailbox[move.dst() + 1] = NO_PIECE;
+			this.mailbox[move.dst() - 2] = this.mailbox[move.dst() + 1];
+			this.mailbox[move.dst() + 1] = NO_PIECE;
 		} else {
 			rook_mask = square_bits(Rank(move.src() >> 3), FILE_H);
 			rook_mask |= rook_mask >> 2;
-			mailbox[move.dst() + 1] = mailbox[move.dst() - 1];
-			mailbox[move.dst() - 1] = NO_PIECE;
+			this.mailbox[move.dst() + 1] = this.mailbox[move.dst() - 1];
+			this.mailbox[move.dst() - 1] = NO_PIECE;
 		}
-		piece_boards[OCC(side)] ^= square_bits(move.src()) ^ square_bits(move.dst()) ^ rook_mask;
-		piece_boards[KING] ^= square_bits(move.src()) | square_bits(move.dst());
-		piece_boards[ROOK] ^= rook_mask;
+		this.piece_boards[OCC(side)] ^= square_bits(move.src()) ^ square_bits(move.dst()) ^ rook_mask;
+		this.piece_boards[KING] ^= square_bits(move.src()) | square_bits(move.dst());
+		this.piece_boards[ROOK] ^= rook_mask;
 	} else {
 		// Get piece that is moving
-		uint8_t piece = mailbox[move.dst()] & 0b111;
-		// Update mailbox repr first
-		mailbox[move.src()] = mailbox[move.dst()];
-		mailbox[move.dst()] = prev.prev_piece();
+		uint8_t piece = this.mailbox[move.dst()] & 0b111;
+		// Update this.mailbox repr first
+		this.mailbox[move.src()] = this.mailbox[move.dst()];
+		this.mailbox[move.dst()] = prev.prev_piece();
 		// Update piece and occupancy bitboard
 		piece_boards[piece] ^= square_bits(move.src()) | square_bits(move.dst());
 		piece_boards[OCC(side)] ^= square_bits(move.src()) | square_bits(move.dst());
@@ -335,8 +336,8 @@ void Board::unmake_move() {
 	// 	update_control(move.dst(), move.src());
 	// Update EP square
 	ep_square = prev.prev_ep();
-	// Update castling rights
-	castling = prev.prev_castling();
+	// Update this.castling rights
+	this.castling = prev.prev_this.castling();
 }
 
 Bitboard Board::hash() const {
@@ -344,7 +345,7 @@ Bitboard Board::hash() const {
 	// for (int i = 0; i < 6; i++) {
 	// 	hash ^= pieces[i];
 	// }
-	// hash ^= castling;
+	// hash ^= this.castling;
 	// hash ^= ep_square;
 	// hash ^= control[0];
 	// hash ^= control[1];
